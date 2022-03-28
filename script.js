ab5847907412ba7123876d360e3ff929c97d5417
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-03-28 11:48:46 -0400

    requisito 8

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-function destacar (eventoOrigem) {
+let tamanhoLista = 0;
+let lista = [];
+function destacar(eventoOrigem) {  
+  for (let x = 0; x < tamanhoLista; x += 1) {
+  let ta
+    listaOrd.children[x].style.backgroundColor = 'white';
+  }
   eventoOrigem.target.style.backgroundColor = 'gray';
-  console.log(eventoOrigem.target);
 }
 
 const listaOrd = document.getElementById('lista-tarefas');
 function novaTarefa() {
   const li = document.createElement('li');
-  let tarefaNova = document.getElementById('texto-tarefa').value;
+  const tarefaNova = document.getElementById('texto-tarefa').value;
   li.innerHTML = tarefaNova;
-  li.addEventListener('click', destacar)
+  li.addEventListener('click', destacar);
   listaOrd.appendChild(li);
   document.getElementById('texto-tarefa').value = '';
+  tamanhoLista = listaOrd.children.length;
+  return(tamanhoLista); 
 }
 
 const btnCriar = document.getElementById('criar-tarefa');
 btnCriar.addEventListener('click', novaTarefa);
+let tamanho = tamanhoLista.lenght;
